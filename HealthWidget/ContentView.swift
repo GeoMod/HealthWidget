aa487ade49994fd64a94b24a9e7e65b7838b6415
Dan O'Leary <olearydaniel@me.com>
2021-02-18 18:12:16 -0600

    Initial Commit

---
+++ HealthWidget/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HealthWidget
+//
+//  Created by Daniel O'Leary on 2/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HealthWidget/HealthWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthWidgetApp.swift
+//  HealthWidget
+//
+//  Created by Daniel O'Leary on 2/18/21.
+//
+
+import SwiftUI
+
+@main
+struct HealthWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
